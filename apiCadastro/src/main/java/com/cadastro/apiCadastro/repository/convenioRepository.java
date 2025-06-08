0e90d67f84a7f52a54f9fc7d013fc7f37d22afc5
Dyzer4 <88802687+Dyzer4@users.noreply.github.com>
2025-06-08 19:41:44 -0300

    Delete apiCadastro directory

--- apiCadastro/src/main/java/com/cadastro/apiCadastro/repository/convenioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cadastro.apiCadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cadastro.apiCadastro.entity.convenio;
-
-public interface convenioRepository extends JpaRepository<convenio, Integer> {
-
-}
--- apiCadastro/src/main/java/com/cadastro/apiCadastro/repository/usuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cadastro.apiCadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cadastro.apiCadastro.entity.usuario;
-
-public interface usuarioRepository extends JpaRepository<usuario, Integer> {
-
-}
