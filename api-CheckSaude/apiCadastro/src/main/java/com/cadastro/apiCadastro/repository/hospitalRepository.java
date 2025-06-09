84337beaf3a3a51dec8a78bb453f8fea6e7e0b5a
Dyzer4 <88802687+Dyzer4@users.noreply.github.com>
2025-06-08 21:17:37 -0300

    Delete api-CheckSaude/apiCadastro directory

--- api-CheckSaude/apiCadastro/src/main/java/com/cadastro/apiCadastro/repository/hospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cadastro.apiCadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cadastro.apiCadastro.entity.hospital;
-
-public interface hospitalRepository extends JpaRepository<hospital, Integer>{
-
-}
